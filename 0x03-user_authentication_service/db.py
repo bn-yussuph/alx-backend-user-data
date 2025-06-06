1d2824677c867d3e1caa9817669d541a9d156fed
bn-yussuph <sulehajjaajj@gmail.com>
2025-06-06 09:01:47 +0000

    change user_id args in update_ser from str to int

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -61,7 +61,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: str, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """ Update a user
         Args: kwargs
         Return: None
